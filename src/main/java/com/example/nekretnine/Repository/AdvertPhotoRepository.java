651665b948111c44953389e2de2a4c9d7627ca11
Amina Puce <amina.puce18@gmail.com>
2019-02-05 22:58:51 +0100

    update advert and advert photos

--- src/main/java/com/example/nekretnine/Repository/AdvertPhotoRepository.java
+++ src/main/java/com/example/nekretnine/Repository/AdvertPhotoRepository.java
@@ -4,5 +4,9 @@ import com.example.nekretnine.Model.Advert;
 import com.example.nekretnine.Model.AdvertPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface AdvertPhotoRepository extends JpaRepository<AdvertPhoto, Long> {
+    long deleteByAdvertId(long advertId);
 }
